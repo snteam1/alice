ba6f68ed57a168c7a3ab1215f666494c4d75ea70
snteam1 <tboy26250@gmail.com>
2025-03-16 11:52:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = API_ID
-API_HASH = "API_HASH"
+API_ID = 28245881
+API_HASH = "1481f1fc94c0c313a534d27a1e81bb65"
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "BOT_TOKEN"
+BOT_TOKEN = "7206839158:AAH742LDUEZAMNYBKZ5vmomk4sguFilMP6o"
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = "MONGO_DB_URI"
+MONGO_DB_URI = "mongodb+srv://tboy26250:tboy26250@cluster0.dqnxc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0I"
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = LOG_GROUP_ID
+LOG_GROUP_ID = -1002002123102
 
 # Get this value from @ultron2_robot on Telegram by /id
-OWNER_ID = OWNER_ID
+OWNER_ID = 5915456351
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = "https://t.me/thanos_pro"
-SUPPORT_GROUP = "https://t.me/thanosprosss"
+SUPPORT_CHANNEL = "https://t.me/bounty_game_prediction0"
+SUPPORT_GROUP = "https://t.me/bounty_game_prediction0"
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -62,7 +62,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 
 # Get your pyrogram v2 session from Replit
-STRING1 = "STRING_SESSION"
+STRING1 = "BQFMCFMAWOjbLZhnUBZzG16dpOyGAjnm79xmr_lU2EfQc2hkAM-L2pdPcl4bTQzXhCkvkjHcIq4FLqq3qcOlVmY788gTqr0XL4q3QHlm2MbMwSZk7JBMhXSSmtRnEzH-gnooaoc7-w7pFDpYFvr3W3sOVX5C1zYnG4bdABDm8jpWIM30h8ZOj651Q2DyXVN1CbNPsofHnirRHzeUkVTHlE69P7_3hNOySVnkbc0Z7BTZBrcdl7V8GbDtat46-SgIvS5winkzqRwgif_0LMXs6KZQzLo1Q9k41n8oYoARY5asTitnixGiODUt0Gts-f_m6CYp5Jx1tIlHhy2BhKZ36x2Q9cGfKQAAAAFyxvlSAA"
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -77,12 +77,12 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+START_IMG_URL = "https://graph.org/file/0abc7d5de013d19207045-93b3586d6f716c1bdf.jpg"
 
-PING_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+PING_IMG_URL = "https://graph.org/file/0abc7d5de013d19207045-93b3586d6f716c1bdf.jpg"
 
 PLAYLIST_IMG_URL = "https://graph.org/file/763a841a2ad5cbb1e2fc5.jpg"
-STATS_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+STATS_IMG_URL = "https://graph.org/file/0abc7d5de013d19207045-93b3586d6f716c1bdf.jpg"
 TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
 TELEGRAM_VIDEO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
